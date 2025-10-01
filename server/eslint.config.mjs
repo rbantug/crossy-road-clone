d8c8277eab95a716ab5bf2fa81932bfec99ea9ae
rbantug <ronbantug22@gmail.com>
2025-10-01 15:14:56 +0800

    feat: added eslint, prettier and jsdoc to server

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,6 +1,6 @@
 import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
