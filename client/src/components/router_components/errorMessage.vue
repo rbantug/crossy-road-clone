9f9927e56dfb9fa7c0009cc23c288a3a74462e1d
rbantug <ronbantug22@gmail.com>
2025-10-02 23:06:39 +0800

    style: updated the styling in errorMessage component

--- client/src/components/router_components/errorMessage.vue
+++ client/src/components/router_components/errorMessage.vue
@@ -1,4 +1,4 @@
 <template>
     <h1>This page does not exist</h1>
-    <p>Go back to <router-link to="/home">Home</router-link></p>
+    <p>Go back to <router-link to="/home" class="text-blue-600 underline underline-offset-2 cursor-pointer">Home</router-link></p>
 </template>
