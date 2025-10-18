8de764992f38c42ede67c340f9ca3930fffa7bd1
rbantug <ronbantug22@gmail.com>
2025-10-19 00:13:24 +0800

    style: removed unnecessary code

--- client/src/stores/useMap.js
+++ client/src/stores/useMap.js
@@ -3,8 +3,6 @@ import { defineStore } from 'pinia'
 
 import { ref, computed } from 'vue'
 
-import { generateRows } from '@/components/utils/generateRows'
-
 export const useMapStore = defineStore('map', () => {
   const metadata = ref([])
 
@@ -26,19 +24,6 @@ export const useMapStore = defineStore('map', () => {
     metadata.value.push(...val)
   }
 
-
-  function addRow() {
-    const newMetaData = generateRows(10)
-    const startIndex = metadata.value.length
-
-    newMetaData.forEach((data, index) => {
-      const rowIndex = startIndex + index + 1
-      data.id = rowIndex
-    })
-
-    metadata.value.push(...newMetaData)
-  }
-
   function addVehicleRef(rowIndex, vehicleIndex, vehicleRef) {
     const row = metadata.value[rowIndex - 1].vehicles
 
@@ -49,7 +34,6 @@ export const useMapStore = defineStore('map', () => {
     getMetadata,
     updateMetadata,
     pushNewMetadata,
-    addRow,
     addVehicleRef,
     
   }
