16753e5ba0230fe2e4fb45dea4a32cc260b8b658
rbantug <ronbantug22@gmail.com>
2025-11-04 01:09:19 +0800

    style: transferred fixtures folder to __test__ folder

---
+++ server/__test__/fixtures/makeFakeRoom.js
@@ -0,0 +1,29 @@
+import { nanoid } from 'nanoid';
+
+export default function makeFakeRoom(overrides) {
+  /**
+   * @type { import("../../../globalCustomTypes").RoomSchema }
+   */
+  const roomData = {
+    room_id: nanoid(),
+    player: [],
+    map: [],
+    lobbyUrl: nanoid(7),
+    gameUrl: nanoid(9),
+    tileSet: new Set(),
+    readyCount: 0,
+    activeAlivePlayers: null,
+    hasNewRoom: false,
+    newLobbyUrl: null,
+    gameParameters: {
+      duration: 5,
+      enableDuration: true,
+      lives: 3,
+    },
+  };
+
+  return {
+    ...roomData,
+    ...overrides,
+  };
+}
