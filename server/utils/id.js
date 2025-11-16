724fa0f7ccd5cc94463cb85fb97daf84437a119e
rbantug <ronbantug22@gmail.com>
2025-11-16 21:05:57 +0800

    feat: added new method that checks the length of the roomId

--- server/utils/id.js
+++ server/utils/id.js
@@ -1,9 +1,10 @@
 import { nanoid } from "nanoid";
 
 const identity = Object.freeze({
-    makeRoomId: () => nanoid(),
+    makeRoomId: () => nanoid(), // 21 char
     makeLobbyUrl: () => nanoid(7),
     makeGameUrl: () => nanoid(9),
+    roomIdIsValid: (id) => id.trim().length === 21
 })
 
 export default identity
