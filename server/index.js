17242727e7a47d18bdc6c33356fe87b17bd03380
rbantug <ronbantug22@gmail.com>
2025-11-26 23:47:16 +0800

    [KAN-32] refactor: "state" and "data" are now not used in the entire backend

--- server/index.js
+++ server/index.js
@@ -1,22 +1,15 @@
 //@ts-check
-import { randomInt } from 'crypto';
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
 import * as Types from './customTypes.js';
-import {
-  createGameUrl,
-  createLobbyUrl,
-  createRoomId,
-  data,
-  outputPlayerData,
-} from './data.js';
 import gameListener from './listeners/game.js';
 import homeAndLobbyListener from './listeners/homeAndLobby.js';
 import playerService from './use-cases/player/index.js';
 import roomService from './use-cases/room/index.js'
 import { utilAddRow } from './utils/generateRows.js';
+import identity from './utils/id.js';
 import { utilRemoveClient } from './utils/removeClient.js';
 
 
@@ -36,48 +29,23 @@ server.listen(3000, () => {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  /**
-   * client's state
-   * @type {Types.state}
-   */
-
-  const state = {
-    clientIndex: null,
-    gameStart: false,
-    roomId: null,
-    lobbyUrl: null,
-    gameUrl: null,
-  };
-
   homeAndLobbyListener({
     io,
     socket,
     roomService,
     playerService,
-    state,
-    data,
     utilRemoveClient,
     utilAddRow,
-    createRoomId,
-    createLobbyUrl,
-    createGameUrl,
-    randomInt,
-    outputPlayerData,
+    createLobbyUrl: identity.makeLobbyUrl,
+    createGameUrl: identity.makeGameUrl,
   });
 
   gameListener({
     io,
     socket,
-    roomService,
     playerService,
-    state,
-    data,
-    utilRemoveClient,
+    roomService,
     utilAddRow,
-    createRoomId,
-    createLobbyUrl,
-    createGameUrl,
-    randomInt,
-    outputPlayerData,
+    createLobbyUrl: identity.makeLobbyUrl,
   });
 });
